3a0440766201a776464d7eb53d08fd2529b922a5
Zachary Bush <zmbush@berkeley.edu>
2012-04-22 01:29:21 -0700

    Working on the idea of heuristics.

--- justbrute.py
+++ justbrute.py
@@ -10,18 +10,19 @@ def lenHeuristic(g, node):
   return len(g[node])
 
 def heu(g, node):
-  print "Evaluating for node:",node
+  # print "Evaluating for node:",node
   adjacent = g[node]
   score = len(adjacent)
-  print "Score so far:",score
+  # print "Score so far:",score
   maxAdd = 0
   for neighbor in adjacent:
-    print "\tChecking neighbor:",neighbor
-    maxAdd = max(maxAdd, len(g[neighbor]) - 1)
-    print "maxAdd is now:",maxAdd
+    # print "\tChecking neighbor:",neighbor
+    # score += (len(g[neighbor]) - 1)*.01
+    maxAdd = max(maxAdd, (len(g[neighbor]) - 1) * 0.5)
+    # print "maxAdd is now:",maxAdd
   score += maxAdd
-  print "Final Score:",score
-  print
+  # print "Final Score:",score
+  # print
   return score
 
 if __name__ == "__main__":
