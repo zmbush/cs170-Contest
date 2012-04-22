e5d4ba5853463f18f6cb002053d9da48420d34be
Zachary Bush <zmbush@berkeley.edu>
2012-04-22 16:57:33 -0700

    Update to work with networkx. randgraph now takes filename, #nodes, edge probability

--- randgraph.py
+++ randgraph.py
@@ -1,18 +1,24 @@
 import sys
 import random
+import networkx as nx
 
-if len(sys.argv) <= 2:
+if len(sys.argv) <= 3:
   print "Argument Required"
 else:
   nodes = 0
   degree = 0
+  fname = sys.argv[1]
   try:
-    nodes = int(sys.argv[1])
-    degree = int(sys.argv[2])
+    nodes = int(sys.argv[2])
+    degree = float(sys.argv[3])
   except ValueError:
     print "You must specify an integer"
     sys.exit(-1)
-
+  print nodes
+  print degree
+  G = nx.fast_gnp_random_graph(nodes, degree)
+  nx.write_adjlist(G, fname) 
+  """
   graph = []
   for n in range(nodes):
     node = []
@@ -27,4 +33,4 @@ else:
   
   for i in range(nodes):
     print i,' '.join(str(x) for x in graph[i])
-  
+  """
