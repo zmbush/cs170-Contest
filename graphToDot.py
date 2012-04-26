e29ae8774b1b334c05ec1213cb4b60fd99d1f8e8
Zachary Bush <zmbush@berkeley.edu>
2012-04-26 14:30:49 -0700

    Fix graphToDot to reflect move

--- graphToDot.py
+++ graphToDot.py
@@ -6,5 +6,5 @@ if __name__ == "__main__":
   if len(sys.argv) <= 1:
     print "Require filename"
   else:
-    G = nx.read_adjlist(sys.argv[1] + '.adjlist')
+    G = nx.read_adjlist('graphs/' + sys.argv[1] + '.adjlist')
     nx.write_dot(G, sys.argv[1] + '.dot')
