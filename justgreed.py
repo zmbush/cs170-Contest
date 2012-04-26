8ef62e069cb34167fd7926e0856d23514cd7237c
Zachary Bush <zmbush@berkeley.edu>
2012-04-26 14:20:50 -0700

    Add documentation to justgreed

--- justgreed.py
+++ justgreed.py
@@ -1,32 +1,34 @@
-# from graph import Graph
 from solver import solve
-import solver
 from verify import verify
+
 import networkx as nx
+import solver
 import sys
 
+"""
+The worst solution. 
+"""
 def nullHeuristic(g, node, touched):
   return 0
 
+"""
+Very basic greedy algorithm
+"""
 def lenHeuristic(g, node, touched):
   untouched = [l for l in g.neighbors(node) if l not in touched] 
   return len(untouched)
 
+"""
+Greedy with one turn look-ahead
+"""
 def oracleHeuristic(g, node, touched):
-  # print "Evaluating for node:",node
   untouched = [l for l in g.neighbors(node) if l not in touched]
   score = len(untouched)
-  # print "Score so far:",score
   maxAdd = 0
   for neighbor in untouched:
-    # print "\tChecking neighbor:",neighbor
-    # score += (len(g[neighbor]) - 1)*.01
     u = [l for l in g.neighbors(neighbor) if l not in touched]
     maxAdd = max(maxAdd, (len(u) - 1) * 0.5)
-    # print "maxAdd is now:",maxAdd
   score += maxAdd
-  # print "Final Score:",score
-  # print
   return score
 
 if __name__ == "__main__":
@@ -36,11 +38,16 @@ if __name__ == "__main__":
     heur = "oracle"
     if len(sys.argv) > 2:
       heur = sys.argv[2]
+    """Read Graph in"""
     g = nx.read_adjlist(sys.argv[1] + '.adjlist')
     print "Solving", sys.argv[1], "with", heur+"Heuristic"
+
+    """Solve For Graph with greedy algorithm"""
     solution = solve(g, heuristic=eval(heur + "Heuristic"), alg="greedy")
     print "Size of solution:",len(solution)
     print "Solution:",solution
+
+    """Verify the correctness of the solution"""
     if verify(g, solution):
       print "Valid!"
     else:
